988555abc0f0761e0d10fb0e710e3e6ab9b791f6
project2479 <jaywrsnp6@gmail.com>
2024-08-03 17:10:22 +0800

    added footer

--- src/components/residents/Footer.jsx
+++ src/components/residents/Footer.jsx
@@ -1,11 +1,30 @@
+import PhonelinkRingIcon from "@mui/icons-material/PhonelinkRing";
+import SosIcon from "@mui/icons-material/Sos";
 function Footer() {
 	return (
 		<>
-			<div className="bg-brown-700 py-4 p-2 mt-10">
-				<p className="text-left text-sm text-white mb-4 font-bold">Be safe, everyone.</p>
-				<p className="text-left text-xs text-white">
-					Any emergency will be contact to your Municipal Offcials at this number <span className="font-bold">09123456789</span>
+			<div className="bg-brown-700 py-4 p-2 mt-10 text-white">
+				<p className="text-center text-sm mb-4 font-bold">
+					Press the SOS button if you need immediate assistance. Be safe, everyone.
 				</p>
+				<div className="flex flex-row">
+					<div className="mr-8 ml-4 bg-red-700 rounded-full p-2 border-4 border-red-100 relative">
+						<SosIcon sx={{ fontSize: 80 }} />
+						<div className="bg-red-600 w-24 rounded-full h-24 top-0 left-0 absolute animate-ping"></div>
+					</div>
+					<div className="flex flex-col justify-evenly">
+						<p className="text-left text-xs">
+							Please avoid prank calls. Our officials are working hard to serve you. <span className="font-bold"></span>
+						</p>
+						<p className="border-2 border-red-50 text-red-50 rounded-full w-fit px-8 py-1.5 mt-2 text-xs flex items-center">
+							<PhonelinkRingIcon
+								fontSize="small"
+								className="mr-2"
+							/>{" "}
+							EMERGENCY
+						</p>
+					</div>
+				</div>
 			</div>
 		</>
 	);
