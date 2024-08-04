92f3b2305ce4c316b7bcc07d38200c3741190372
project2479 <jaywrsnp6@gmail.com>
2024-08-04 13:09:06 +0800

    disable zoom

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-/* eslint-disable no-unused-vars */
+import { useEffect } from "react";
 import NoInterConnection from "./components/NoInternetConnection";
 import Login from "./routes/Login";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
@@ -13,8 +13,40 @@ function Logout() {
 	return <Navigate to="/" />;
 }
 
+const disableZoom = () => {
+	// Disable zoom on mouse wheel
+	window.addEventListener(
+		"wheel",
+		(e) => {
+			if (e.ctrlKey) {
+				e.preventDefault();
+			}
+		},
+		{ passive: false }
+	);
+
+	// Disable zoom on keydown (Ctrl + '+' or '-' or '0')
+	window.addEventListener("keydown", (e) => {
+		if (
+			(e.ctrlKey && (e.key === "+" || e.key === "-" || e.key === "0")) ||
+			(e.key === "Meta" && (e.key === "+" || e.key === "-" || e.key === "0"))
+		) {
+			e.preventDefault();
+		}
+	});
+};
 
 function App() {
+	useEffect(() => {
+		disableZoom();
+
+		return () => {
+			// Cleanup event listeners on unmount
+			window.removeEventListener("wheel", disableZoom);
+			window.removeEventListener("keydown", disableZoom);
+		};
+	}, []);
+
 	return (
 		<>
 			<NoInterConnection>
