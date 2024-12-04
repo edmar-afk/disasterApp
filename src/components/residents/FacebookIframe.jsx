0a70496ca1008b1f3e189c72fb87725738a446ca
project2479 <jaywrsnp6@gmail.com>
2024-12-04 11:38:33 +0800

    new

--- src/components/residents/FacebookIframe.jsx
+++ src/components/residents/FacebookIframe.jsx
@@ -1,26 +1,28 @@
-import MdrrmoHeader from "./MdrrmoHeader";const FacebookIframe = () => {	return (	<>
-		<MdrrmoHeader />
-		<div style={{ textAlign: "center", padding: "20px" }}>
-			<p>The Facebook profile cannot be embedded. Click the link below to visit the profile:</p>
-			<div
-				className="fb-page"
-				data-href="https://web.facebook.com/profile.php?id=100071365517204&amp;_rdc=1&amp;_rdr"
-				data-tabs="timeline"
-				data-width="375"
-				data-height=""
-				data-small-header="false"
-				data-adapt-container-width="true"
-				data-hide-cover="false"
-				data-show-facepile="true">
-				<blockquote
-					cite="https://web.facebook.com/profile.php?id=100071365517204&amp;_rdc=1&amp;_rdr"
-					className="fb-xfbml-parse-ignore">
-					<a href="https://web.facebook.com/profile.php?id=100071365517204&amp;_rdc=1&amp;_rdr">Mdrrmo Lakewood</a>
-				</blockquote>
+import MdrrmoHeader from "./MdrrmoHeader";
+const FacebookIframe = () => {
+	return (
+		<>
+			<MdrrmoHeader />
+			<div style={{ textAlign: "center", padding: "20px" }}>
+				<p>The Facebook profile cannot be embedded. Click the link below to visit the profile:</p>
+				<a
+					href="https://web.facebook.com/profile.php?id=100071365517204&_rdc=1&_rdr"
+					target="_blank"
+					rel="noopener noreferrer"
+					style={{
+						display: "inline-block",
+						backgroundColor: "#4267B2",
+						color: "#fff",
+						padding: "10px 20px",
+						borderRadius: "5px",
+						textDecoration: "none",
+						fontSize: "16px",
+					}}>
+					Open Facebook Profile in Browser
+				</a>
 			</div>
-		</div>
-	</>
-);
+		</>
+	);
 };
 
 export default FacebookIframe;
