b79eb8c087152be522560a9e4ca3086e28d9d83a
project2479 <jaywrsnp6@gmail.com>
2024-12-11 09:20:36 +0800

    new

--- src/components/residents/Footer.jsx
+++ src/components/residents/Footer.jsx
@@ -1,7 +1,9 @@
 import PhonelinkRingIcon from "@mui/icons-material/PhonelinkRing";
 import SosIcon from "@mui/icons-material/Sos";
-import { Link } from "react-router-dom";
+
 function Footer() {
+	const facebookUrl = "https://www.facebook.com/profile.php?id=100071365517204";
+
 	return (
 		<>
 			<div className="bg-brown-700 py-4 p-2 mt-10 text-white">
@@ -10,24 +12,30 @@ function Footer() {
 				</p>
 				<div className="flex flex-row">
 					{/* SOS Icon */}
-					<div className="mr-8 ml-4 bg-red-700 rounded-full p-2 border-4 border-red-100 relative cursor-pointer">
+					<a
+						href={facebookUrl}
+						target="_blank"
+						rel="noopener noreferrer"
+						className="mr-8 ml-4 bg-red-700 rounded-full p-2 border-4 border-red-100 relative cursor-pointer">
 						<SosIcon sx={{ fontSize: 80 }} />
 						<div className="bg-red-600 w-24 rounded-full h-24 top-0 left-0 absolute animate-ping"></div>
-					</div>
+					</a>
 					<div className="flex flex-col justify-evenly">
 						<p className="text-left text-xs">
 							Please avoid doing prank calls. Our officials are working hard to serve you.
 						</p>
 						{/* Facebook Profile link */}
-						<Link
-							to={"mdrrmoSOS"}
+						<a
+							href={facebookUrl}
+							target="_blank"
+							rel="noopener noreferrer"
 							className="border-2 border-red-50 text-red-50 rounded-full w-fit px-8 py-1.5 mt-2 text-xs flex items-center">
 							<PhonelinkRingIcon
 								fontSize="small"
 								className="mr-2"
 							/>{" "}
 							Facebook Profile
-						</Link>
+						</a>
 					</div>
 				</div>
 			</div>
